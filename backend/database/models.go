9ef8a906e49108b8e47f2c8bc032733c1cd539ed
Humberto Correia <correia.humberto@gmail.com>
2025-05-11 12:21:08 +0100

    feat(backend): JSON null types - sqlc overrides

--- backend/database/models.go
+++ backend/database/models.go
@@ -5,15 +5,16 @@
 package database
 
 import (
-	"database/sql"
 	"time"
+
+	null "github.com/guregu/null/v6"
 )
 
 type Post struct {
-	ID        int32          `json:"id"`
-	Title     string         `json:"title"`
-	Image     sql.NullString `json:"image"`
-	Content   string         `json:"content"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
+	ID        int32       `json:"id"`
+	Title     string      `json:"title"`
+	Image     null.String `json:"image"`
+	Content   string      `json:"content"`
+	CreatedAt time.Time   `json:"created_at"`
+	UpdatedAt time.Time   `json:"updated_at"`
 }
--- backend/go.mod
+++ backend/go.mod
@@ -6,6 +6,7 @@ require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.9.1 // indirect
 	github.com/golang-migrate/migrate/v4 v4.18.3 // indirect
+	github.com/guregu/null/v6 v6.0.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
